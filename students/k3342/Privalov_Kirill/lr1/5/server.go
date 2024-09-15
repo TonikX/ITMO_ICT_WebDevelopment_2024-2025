a4df04af31352a89649dba31f2989c679a77c310
hellkirl <kirillprivalov04@icloud.com>
2024-09-15 23:24:13 +0300

    feat: add lab1/5 chore: change the location of docs

--- students/k3342/Privalov_Kirill/lr1/5/server.go
+++ students/k3342/Privalov_Kirill/lr1/5/server.go
@@ -1,20 +1,22 @@
 package main
 
 import (
+	"encoding/json"
+	"html/template"
 	"log"
 	"net/http"
+	"sync"
 )
-
-type Student struct {
-	RecordBook Grade `json:"grade"`
-}
 
 type Grade struct {
 	Subject string `json:"subject"`
 	Grade   int    `json:"grade,omitempty"`
 }
 
-var grades []Grade
+var (
+	gradesMap = make(map[string]int)
+	mu        sync.Mutex
+)
 
 func main() {
 	mux := setUpMux()
@@ -26,20 +28,75 @@ func main() {
 }
 
 func run(mux *http.ServeMux) error {
-	err := http.ListenAndServe(":8080", mux)
-	return err
+	return http.ListenAndServe(":8080", mux)
 }
 
 func setUpMux() *http.ServeMux {
 	mux := http.NewServeMux()
-
+	mux.HandleFunc("/", handleHomePage)
+	mux.HandleFunc("/new-grade", handleNewGrade)
 	return mux
 }
 
-func handleHomePage() {}
+func handleHomePage(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	tmpl := `
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Grades</title>
+</head>
+<body>
+	<h1>All Grades</h1>
+	<table border="1">
+		<tr>
+			<th>Subject</th>
+			<th>Grade</th>
+		</tr>
+		{{range $subject, $grade := .}}
+		<tr>
+			<td>{{$subject}}</td>
+			<td>{{$grade}}</td>
+		</tr>
+		{{else}}
+		<tr>
+			<td colspan="2">No grades available</td>
+		</tr>
+		{{end}}
+	</table>
+</body>
+</html>
+`
+	t := template.Must(template.New("grades").Parse(tmpl))
+	if err := t.Execute(w, gradesMap); err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
 
 func handleNewGrade(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		http.Error(w, "Method is not allowed", http.StatusMethodNotAllowed)
+		return
 	}
+
+	var newGrade Grade
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&newGrade); err != nil {
+		http.Error(w, "Invalid JSON", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	if newGrade.Subject == "" || newGrade.Grade < 0 || newGrade.Grade > 5 {
+		http.Error(w, "Invalid grade data", http.StatusBadRequest)
+		return
+	}
+
+	mu.Lock()
+	gradesMap[newGrade.Subject] = newGrade.Grade
+	mu.Unlock()
+
+	w.WriteHeader(http.StatusNoContent)
 }
