fad87c03f81710c16f3d2155c47e54c28ebfc660
Mayonnaise-Slap <368758@niuitmo.ru>
2024-10-10 22:29:34 +0300

    added app

--- students/k3343/laboratory_works/Samsonov_Alexander/Lr2/Lr2/urls.py
+++ students/k3343/laboratory_works/Samsonov_Alexander/Lr2/Lr2/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for hwboard project.
+URL configuration for Lr2 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
--- students/k3343/laboratory_works/Samsonov_Alexander/Lr2/Lr2/wsgi.py
+++ students/k3343/laboratory_works/Samsonov_Alexander/Lr2/Lr2/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hwboard project.
+WSGI config for Lr2 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hwboard.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Lr2.settings")
 
 application = get_wsgi_application()
