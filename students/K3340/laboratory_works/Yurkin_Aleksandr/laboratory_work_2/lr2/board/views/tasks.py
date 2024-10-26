909f39892a20e839c2c186f625b737ad26b888a2
AlexYrkn <61319606+AlexYrkn@users.noreply.github.com>
2024-10-26 17:40:23 +0300

    lr2 is done

--- students/K3340/laboratory_works/Yurkin_Aleksandr/laboratory_work_2/lr2/board/views/tasks.py
+++ students/K3340/laboratory_works/Yurkin_Aleksandr/laboratory_work_2/lr2/board/views/tasks.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, CreateView
+from django.utils import timezone
 
 from ..models import Student, Task, Assignment
 from lr2.settings import Statuses
@@ -38,3 +39,25 @@ class TasksView(TemplateView):
         context['data'] = data
 
         return context
+
+class TaskDetailView(DetailView):
+    model = Task
+    template_name = 'task_details.html'
+    context_object_name = 'task'
+
+
+class AssignmentCreateView(CreateView):
+    model = Assignment
+    fields = ['text']
+    template_name = 'assignment_form.html'
+    success_url = '/tasks'
+
+    def form_valid(self, form):
+        form.instance.student_id = self.request.user.id
+        form.instance.received_at = timezone.now()
+        form.instance.status = Statuses.PENDING
+
+        task_id = self.kwargs.get('task_id')
+        form.instance.task_id = task_id
+
+        return super().form_valid(form)
