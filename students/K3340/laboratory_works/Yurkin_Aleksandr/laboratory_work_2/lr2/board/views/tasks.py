94892aa736d0052a18bd72ef545fae9ca9b62849
AlexYrkn <61319606+AlexYrkn@users.noreply.github.com>
2024-10-31 11:28:50 +0300

    added menu, pagination and search

--- students/K3340/laboratory_works/Yurkin_Aleksandr/laboratory_work_2/lr2/board/views/tasks.py
+++ students/K3340/laboratory_works/Yurkin_Aleksandr/laboratory_work_2/lr2/board/views/tasks.py
@@ -1,9 +1,12 @@
 from django.shortcuts import redirect
 from django.views.generic import TemplateView, DetailView, CreateView
+from django.core.paginator import Paginator
 from django.utils import timezone
+from django.db.models import Q
 
 from ..models import Student, Task, Assignment
 from lr2.settings import Statuses
+
 
 class TasksView(TemplateView):
     template_name = 'tasks.html'
@@ -17,26 +20,34 @@ class TasksView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-
         user_id = self.request.session.get('user_id')
 
         if not user_id:
             return context
 
         student = Student.objects.get(pk=user_id)
+
+        search_query = self.request.GET.get('search', '')
+
         tasks = Task.objects.filter(student_classes=student.student_class).order_by('expire_at')
+        if search_query:
+            tasks = tasks.filter(Q(title__icontains=search_query))
+
+        paginator = Paginator(tasks, 2)
+        page_number = self.request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
 
         data = []
-
-        for task in tasks:
+        for task in page_obj:
             data.append({
                 'task': task,
                 'assignment': Assignment.objects.filter(student=student, task=task).first()
             })
 
-
         context['student'] = student
         context['data'] = data
+        context['page_obj'] = page_obj
+        context['search_query'] = search_query
 
         return context
 
