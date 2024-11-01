b423938d8a16fdc03d0a0c7d9a4b3f2deaf39ec7
Di0Zavr <viktorsergeev08@gmail.com>
2024-11-02 00:08:05 +0300

    feat: init project, make models

---
+++ students/k3341/laboratory_works/Sergeev_Victor/lr2/scienceconf/science/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+# wanna remove
+class User(AbstractUser):
+    name = models.CharField(max_length=50)
+
+class Participant(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    profile_picture = models.URLField(blank=True, null=True)
+    country = models.CharField(max_length=50)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class Conference(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=500, blank=True, null=True)
+    participate_conditionals = models.CharField(max_length=500, blank=True, null=True)
+    location = models.CharField(max_length=150)
+    date_of_start = models.DateField()
+    date_of_finish = models.DateField()
+    auditors = models.ManyToManyField(Participant, through='ConferenceAuditor', related_name='conference_auditors')
+    speakers = models.ManyToManyField(Participant, through='ConferenceSpeaker', related_name='conference_speakers')
+
+class ConferenceAuditor(models.Model):
+    conference = models.ForeignKey(Conference, on_delete=models.CASCADE)
+    auditor = models.ForeignKey(Participant, on_delete=models.CASCADE)
+
+class ConferenceSpeaker(models.Model):
+    conference = models.ForeignKey(Conference, on_delete=models.CASCADE)
+    speaker = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    speaking_topic = models.CharField(max_length=200)
+    date_of_start = models.DateField()
+    date_of_finish = models.DateField()
+
+class Review(models.Model):
+    grade = models.PositiveSmallIntegerField() # out of 10
+    likes = models.PositiveIntegerField(default=0)
+    dislikes = models.PositiveIntegerField(default=0)
+    description = models.CharField(max_length=1000)
+    conference = models.ForeignKey(Conference, on_delete=models.CASCADE)
+    author = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    date = models.DateField()
+
+class Commentary(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    author = models.ForeignKey(Participant, on_delete=models.CASCADE)
+    likes = models.PositiveIntegerField(default=0)
+    dislikes = models.PositiveIntegerField(default=0)
+    description = models.CharField(max_length=600)
