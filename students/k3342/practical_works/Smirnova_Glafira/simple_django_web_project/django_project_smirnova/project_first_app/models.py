ef7a733e25df0a55b55ab49b60402e031cd2848d
GlafiraSmiirnova <glafira.smirnova@gmail.com>
2024-12-01 19:14:25 +0300

    practice(docs): implement practical tasks and documentation

--- students/k3342/practical_works/Smirnova_Glafira/simple_django_web_project/django_project_smirnova/project_first_app/models.py
+++ students/k3342/practical_works/Smirnova_Glafira/simple_django_web_project/django_project_smirnova/project_first_app/models.py
@@ -6,11 +6,13 @@ from django.db import models
 class Owner(AbstractUser):
     birth_date = models.DateField(null=True)
     passport_number = models.CharField(max_length=10, default="")
-    cars = models.ManyToManyField('Car', through="Ownership")
     address = models.CharField(max_length=30, null=True)
     last_name = models.CharField(max_length=30, null=False)
     first_name = models.CharField(max_length=30, null=False)
     nationality = models.CharField(max_length=30, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.last_name} {self.first_name}"
 
 
 class Car(models.Model):
@@ -18,18 +20,28 @@ class Car(models.Model):
     brand = models.CharField(max_length=20, null=False)
     model = models.CharField(max_length=20, null=False)
     color = models.CharField(max_length=30, null=True)
-    car_owner = models.ManyToManyField(Owner, through='Ownership')
+    owners = models.ManyToManyField(
+        "Owner",
+        through="Ownership",
+        related_name="cars",
+    )
+
+    def __str__(self) -> str:
+        return f"{self.brand} {self.model}"
 
 
 class DriverLicense(models.Model):
     license_number = models.CharField(max_length=10, null=False)
     type = models.CharField(max_length=10, null=False)
     date_of_issue = models.DateField(null=False)
-    owner_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, related_name="licenses", on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.license_number} {self.owner.last_name} {self.owner.first_name}"
 
 
 class Ownership(models.Model):
     date_start = models.DateField(null=False)
     date_end = models.DateField(null=True)
-    car_id = models.ForeignKey(Car, null=True, on_delete=models.CASCADE)
-    owner_id = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, null=True, on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete=models.CASCADE)
