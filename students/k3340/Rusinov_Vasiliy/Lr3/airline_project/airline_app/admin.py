341a6e2a12de4d914276a12a7e9c72eeaece74ef
vendatt0r <vasyok03020454@gmail.com>
2024-12-02 17:38:51 +0300

    Загружены практические задания и ЛР №3

---
+++ students/k3340/Rusinov_Vasiliy/Lr3/airline_project/airline_app/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import *
+
+
+admin.site.register(Airline)
+admin.site.register(Airport)
+admin.site.register(Plane)
+admin.site.register(Maintenance)
+admin.site.register(Employee)
+admin.site.register(CrewMember)
+admin.site.register(Crew)
+admin.site.register(Route)
+admin.site.register(Flight)
+admin.site.register(Seat)
+admin.site.register(TransitStop)
---
+++ students/k3340/Rusinov_Vasiliy/Lr3/airline_project/airline_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AirlineAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "airline_app"
